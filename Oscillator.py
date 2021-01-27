bb1478e660a9e648e3de8d4439d4a34be20166e4
RyanCocking <bsrctb@leeds.ac.uk>
2021-01-27 18:33:42 +0000

    Added init and neighbour methods

--- Oscillator.py
+++ Oscillator.py
@@ -1,19 +1,44 @@
+import numpy
+
 class Oscillator:
     """
     A 1D spherical particle. Oscillates due to Brownian motion and a
     harmonic potential. Experiences kinetic switching between four states.
     """
     
-    def __init__():
+    def __init__(self, id=0, position=0.0, origin=0.0, state="A", all_states=["A, B, C, D"]):
         """
         potential
-        state
-        adjacent_states
-        position
-        displacement
-        velocity
         """
-        pass
+        self.id = id
+        self.position = position
+        self.displacement = self.position - origin
+        self.state = state
+        self.adjacent_states = get_adjacent_states(all_states)
+    
+    def get_adjacent_states(all_states):
+        """
+        Input:  1D list of state names
+        Return: 2D list of adjacent states
+        
+        Assuming each state only has two neighbours and the kinetic network
+        is a cycle, i.e.:
+        
+        B-----C
+        |     |
+        A-----D
+        
+        generate neighbours for each state so the function will return:
+        
+        [["D", "B"], ["A", "C"], ["B", "D"], ["C", "A"]]
+        """
+
+        for state in all_states[1:-1]
+            adjacent_states = [ [state[i-1], state[i+1]] ]
+        adjacent_states[0]  = [state[-1], state[1]]
+        adjacent_states[-1] = [state[0], state[-2]]
+        
+        return adjacent_states[:, :]
     
     def state():
         """
